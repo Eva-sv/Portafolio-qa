65fd962a7bc7a47dde3f1e1ac40b34b4b3eab3fe
Eva SV <evasauraklix@hotmail.com>
2026-03-26 11:19:48 +0100

    delete.sql
    Cambios delete.sql

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,12 +1,3 @@
--- Change the email of the user with id 10 to lauratest@gmail.com
-UPDATE form
-SET email = 'lauratest@gmail.com'
-WHERE id = 10;
-
--- Change the age of the user with id 2 from 20 to 21
-UPDATE form
-SET age = 21
-WHERE id = 2;
 
 -- Delete all records with age 30
 DELETE FROM form_delete WHERE age = 30;
