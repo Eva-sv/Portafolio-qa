3fdc60e1ad925f019c82e0568b652229c6fcc8ff
Eva SV <evasauraklix@hotmail.com>
2026-04-01 20:14:01 +0200

    Update delete.sql
    Quitar el form_delete

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,6 +1,6 @@
 
 -- Delete all records with age 30
-DELETE FROM form_delete WHERE age = 30;
+DELETE FROM form WHERE age = 30;
 
 -- Delete all records with the name 'pedro'
-DELETE FROM form_delete WHERE name = 'pedro';
+DELETE FROM form WHERE name = 'pedro';
